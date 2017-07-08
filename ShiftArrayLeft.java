7b836fa3ba23921eb5bf8aee9b55a855d61dc4e9
Nathan <nepoche@yahoo.com>
2017-07-08 19:30:17 -0400

    add problem statement and analysis

--- ShiftArrayLeft.java
+++ ShiftArrayLeft.java
@@ -4,6 +4,26 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/*
+ | - - - - - - - - - - - - - - - - - - - - - - - Problem Statement - - - - - - - - - - - - - - - - - - - - - - - - - - |
+ | A left rotation operation on an array of size n shifts each of the array's elements 1 unit to the left.             |
+ | For example, if left rotations are performed on array [1, 2, 3, 4, 5], then the array would become [3, 4, 5, 1, 2]. |
+ | Given an array of n integers and a number, d, perform d left rotations on the array.                                |
+ | Then print the updated array as a single line of space-separated integers.                                          |
+ | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+
+ | - - - - - - - - - - - - - - - - - - - - - - Analysis - - - - - - - - - - - - - - - - - - - - - - - - - -|
+ | The naive solution requires O(n) extra space complexity (simply create a new array and copy elements).  |
+ |                                                                                                         |
+ | A naive solution with only O(1) space complexity would involve multiple shifts of 1,                    |
+ | resulting in O(shifts * n) time.                                                                        |
+ |                                                                                                         |
+ | This solution utilizes the greatest common factor between array size and shift number, since the GCF of |
+ | these numbers is equal to the number of cyclic groups with all unique elements. This means we can have  |
+ | a solution that runs in O(n) time with only O(1) space complexity.                                      |
+ | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+*/
+
 public class Solution {
     
     public static int gcf(int a, int b) {
